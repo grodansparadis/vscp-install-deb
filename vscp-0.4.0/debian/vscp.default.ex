d402493a4165d140bb6f9c1356b6f6a6f7035ac9
Ake Hedman <akhe@grodansparadis.com>
2013-10-15 22:19:32 +0200

    Added libs and tarbvals for 0.4.0

---
+++ vscp-0.4.0/debian/vscp.default.ex
@@ -0,0 +1,10 @@
+# Defaults for vscp initscript
+# sourced by /etc/init.d/vscp
+# installed at /etc/default/vscp by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
